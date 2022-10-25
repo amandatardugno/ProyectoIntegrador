39c62c4abd82c983442972a4474486e0d42c7047
Amanda <amandatardugno@gmail.com>
2022-10-25 16:07:12 -0300

    Comienzo con backend
    anexamos el nombre a la base de datos

--- ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/amanda/Controler/PersonaControler.java
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/amanda/Controler/PersonaControler.java
@@ -4,6 +4,7 @@ import com.portfolio.amanda.Entity.Persona;
 import com.portfolio.amanda.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaControler {
     @Autowired IPersonaService ipersonaService;
     
@@ -48,5 +50,10 @@ public class PersonaControler {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPerson(){
+        return ipersonaService.findPersona((long)1);
     }
 }
