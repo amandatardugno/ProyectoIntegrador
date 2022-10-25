3bc5eb4df7dbc8d275bb168180a047eb3217b770
Amanda <amandatardugno@gmail.com>
2022-10-25 17:15:11 -0300

    login
    creacion del componente login

--- ProyectoIntegrador/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador/FrontEnd/src/app/app.module.ts
@@ -13,7 +13,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
