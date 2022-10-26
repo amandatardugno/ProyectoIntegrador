ca6f2f5bf588f69301a75c0a3e3eca1cf72008ce
Amanda <amandatardugno@gmail.com>
2022-10-26 00:20:39 -0300

    Jwt
    terminamos el package de jwt

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/amanda/Security/Service/RolService.java
@@ -0,0 +1,25 @@
+
+package com.portfolio.amanda.Security.Service;
+
+import com.portfolio.amanda.Security.Entity.Rol;
+import com.portfolio.amanda.Security.Enums.RolNombre;
+import com.portfolio.amanda.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);   
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+}
