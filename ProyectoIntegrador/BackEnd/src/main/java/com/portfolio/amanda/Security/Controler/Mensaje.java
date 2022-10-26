a130f4530a729f071b505e337ddb1897670da806
Amanda <amandatardugno@gmail.com>
2022-10-26 01:41:36 -0300

    trabajando en backend
    trabajamos en la carpeta security

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/amanda/Security/Controler/Mensaje.java
@@ -0,0 +1,26 @@
+package com.portfolio.amanda.Security.Controler;
+
+public class Mensaje {
+
+    private String mensaje;
+
+    //Constructores
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //Getter y Setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+
+}
